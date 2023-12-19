fe48c01fccca12af958d6403c5f8f900b5c8f1a6
Stefan-Iliuta <117430548+Stefan-Iliuta@users.noreply.github.com>
2023-12-19 17:36:15 +0200

    Delete CourseOperations.java

--- CourseOperations.java
+++
@@ -1,9 +0,0 @@
-public interface CourseOperations {
-    public void UpdateProfessor(Professor p);
-    public void AddStudent(Student student);
-    public void RemoveStudent(Student student);
-    public void UpdateStudent(Student student);
-    public void UpdateCourse(String name, String description);
-
-
-}
